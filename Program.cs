a7e68814cc47f8b9d2076c78914bf7c513bf101b
zenin5479 <zenin5479@gmail.com>
2026-01-14 22:41:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SyncServer
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
